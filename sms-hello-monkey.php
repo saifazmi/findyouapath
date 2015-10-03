b0cefeea28f58e390af6b7e36e0b46e8219c083b
Saif Azmi <saif.azmi0108@gmail.com>
2015-10-04 00:56:27 +0100

    testing the pathquery-v0.1

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -1,7 +1,24 @@
 <?php
-	header("content-type: text/xml");
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    /* Include twilio-php, the official Twilio PHP Helper Library, 
+    * which can be found at
+    * http://www.twilio.com/docs/libraries
+    */
+ 
+include('Services/Twilio.php');
+
+/* Read the contents of the 'Body' field of the Request. */
+$body = $_REQUEST['Body'];
+
+/* Remove formatting from $body until it is just lowercase 
+characters without punctuation or spaces. */
+$body = preg_replace("/[^A-Za-z0-9]/u", " ", $body);
+$body = trim($body);
+$pathquery = strtolower($body);
+
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"; 
 ?>
+
 <Response>
-	<Message>Hello, Mobile Monkey</Message>
+    <Message><?php echo $pathquery ?></Message>
 </Response>
