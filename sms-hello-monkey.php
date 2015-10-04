ea6368462adff2e963a9258ed3afa510219e1bc0
Saif Azmi <saif.azmi0108@gmail.com>
2015-10-04 07:03:23 +0100

    seems like i'll never grow use to the $ for php variables.

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -81,7 +81,7 @@ function getPathMsg() {
     for($i = 0; $i < count($steps); $i++) {
         
         $step = $steps[$i]["html_instructions"];
-        echo $step;
+        echo "\n$step\n";
         $step = preg_replace("/[\x00-\x1F\x80-\xFF]/", "", $step);
         echo "\n$step\n";
     }
