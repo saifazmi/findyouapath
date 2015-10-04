99025c8e98e96713194350bcfec5c8d27951f574
Saif Azmi <saif.azmi0108@gmail.com>
2015-10-04 06:51:25 +0100

    adding steps to destination.

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -78,12 +78,11 @@ function getPathMsg() {
     /* Parse the steps */
     $steps = $legs[0]["steps"];
     
-    /*
     for($i = 0; $i < count(steps); $i++) {
         $step = steps[$i]["html_instructions"];
         $step = preg_replace("/[\x00-\x1F\x80-\xFF]/", "", $step);
         echo "\n$step\n";
-    }*/
+    }
     
     /*
     for removing all the unicode in html instructions
