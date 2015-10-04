462c459c3d560ce3ef3067635eb99168b90cb207
Saif Azmi <saif.azmi0108@gmail.com>
2015-10-04 01:41:51 +0100

    testing the pathquery-v0.5

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -21,7 +21,7 @@ $pathdata = array(
     'to' => $pathquery[1]
 );
 
-$pathdata = json_encode(array('path' => $pathdata), JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+$pathdata = json_encode(array('path' => $pathdata),  JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
  
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"; 
