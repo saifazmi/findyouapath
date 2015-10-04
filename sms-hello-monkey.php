c7a9292576410ebcc691950d3d7976923efbb342
Saif Azmi <saif.azmi0108@gmail.com>
2015-10-04 06:42:57 +0100

    adding steps to destination.

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -76,7 +76,7 @@ function getPathMsg() {
     echo "\nTime: $duration";
     
     /* Parse the steps */
-    echo strval(count($legs[0]["steps"]);
+    //echo strval(count($legs[0]["steps"]);
     
     /*
     for($i = 0; $i < count(steps); $i++) {
